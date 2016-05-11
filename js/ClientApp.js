d71f7f98d0beae3eeffbd9ac3c132024956a4d17
David Nachman <david@nachman.org>
2016-05-10 22:55:04 -0400

    added npm test

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,14 +1,14 @@
+var React = require('React');
+var ReactDOM = require('ReactDOM');
 var div = React.DOM.div;
 var h1 = React.DOM.h1;
 
-var MyTitle = function(props) {
-
+var MyTitle = function (props) {
     return (
         div(null,
             h1({ style: {color: props.color} }, props.title)
             )
     );
-
 };
 
 var MyTitleFact = React.createFactory(MyTitle);
@@ -19,6 +19,6 @@ var MyFirstComponent = (
         MyTitleFact({title: 'Use props everywhere!', color: 'mediumaquamarine'}),
         MyTitleFact({title: 'Props are the best!', color: 'peru'})
     )
-)
+);
 
 ReactDOM.render(MyFirstComponent, document.getElementById('app'));
